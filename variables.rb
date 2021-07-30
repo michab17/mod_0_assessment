070189e20e8c1d4efb734976b9ce112fb2e7ebae
Micha Bernhard <mib70@pitt.edu>
2021-07-30 13:30:10 -0600

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,15 @@
+animal = "Panda"
+is_cuddly = true
+
+animal = "Zebra"
+
+num_8 = 2.5
+num_3 = 5
+
+quotient = num_3 / num_8
+
+favorite_films = ["Gladiator", "Moon", "Love and Other Drugs"]
+
+favorite_films.shift
+
+puts animal.length
