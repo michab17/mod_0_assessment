4889b022cb854c10ed0225ef9c9dffb0590d65b3
Micha Bernhard <mib70@pitt.edu>
2021-07-30 13:36:18 -0600

    Add laptop hash

---
+++ models/model_a.rb
@@ -0,0 +1,10 @@
+laptop = {
+  "brand" => "Apple",
+  "color" => "gray",
+  "cameras" => 1,
+  "option_buttons" => 2,
+  "is_expensive" => true,
+  "has_numpad" => false,
+  "arrow_keys" => ["Up", "Down", "Left", "Right"],
+  "double_keys" => ["shift", "option", "command"]
+}
