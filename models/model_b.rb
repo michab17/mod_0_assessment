865786f2d45e64f5316d9a642fff14707763157c
Micha Bernhard <mib70@pitt.edu>
2021-07-30 13:42:16 -0600

    Add house hash

---
+++ models/model_b.rb
@@ -0,0 +1,10 @@
+house = {
+  "address" => "123 Street Ave",
+  "wall_color" => "tan",
+  "cats" => 1,
+  "on_floor" => 4,
+  "is_small" => true,
+  "has_sauna" => false,
+  "rooms" => ["kitchen", "bathroom", "bedroom", "living room"],
+  "snacks" => ["crackers", "pretzels", "chocolate"]
+}
